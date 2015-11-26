cd4f7f5b9aa65e446b9f7e7774dd14bee734e177
santosh.khanal55@gmail.com <santoshkhanal@Santoshs-MacBook-Pro.local>
2015-11-26 10:51:03 +0545

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+/* ==========================================================================
+
+    Project: Test Proj
+    Author: XHTMLized
+    Last updated: @@timestamp
+
+   ========================================================================== */
+
+'use strict';
+
+
+var TestProj = {
+
+  /**
+   * Init function
+   */
+  init: function() {
+    TestProj.galleryFn();
+  },
+
+  /**
+   * Example function
+   */
+  galleryFn: function() {
+    var galleryImg = jQuery('.article-block').find('.img');
+    galleryImg.last().addClass('last-el');
+    galleryImg.colorbox({
+      loop: false,
+      rel: 'group01',
+      width: 300,
+      height: 300,
+      closeButton: false
+    });
+    galleryImg.first().click();
+  }
+
+};
+
+jQuery(document).on('cbox_complete', function(){
+  var colorboxTime = setTimeout(jQuery.colorbox.next, 2000);
+  if(jQuery.colorbox.element().hasClass('last-el')) {
+    clearTimeout(colorboxTime);
+    setTimeout(function() {
+      jQuery.colorbox.close()
+    }, 2000);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  TestProj.init();
+});
+
+
